9a72e386a073e250e3c1d075a2c4d2d257863fad
Stefan Mocek <stefan.mocek1@gmail.com>
2022-06-22 15:19:56 +0200

    First version of working app

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
-// index.js
-// where your node app starts
+const express = require('express');
+const app = express();
 
-// init project
-var express = require('express');
-var app = express();
+const cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
-
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/api", (req,res)=>{
+  res.json({"unix": new Date().getTime(), "utc": new Date().toUTCString()})
+})
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+app.get("/api/:date", (req,res)=>{
+  let date_string = req.params.date;
+  console.log(req.params)
+  let odpowiedz = {}
+ if (date_string.includes("-")) {
+    odpowiedz["unix"] = new Date(date_string).getTime(); 
+    odpowiedz["utc"]= new Date(date_string).toUTCString();
+  } else if (!isNaN(date_string)){
+    date_string = parseInt(date_string),
+    odpowiedz["unix"] = new Date(date_string).getTime(); 
+    odpowiedz["utc"]= new Date(date_string).toUTCString();
+  } else {
+    odpowiedz["unix"] = new Date(date_string).getTime()
+    odpowiedz["utc"]= new Date(date_string).toUTCString()
+  }
+  if (!odpowiedz["unix"] || !odpowiedz["utc"]){
+    odpowiedz = {error : "Invalid Date"}
+  }
+  res.json(odpowiedz)
+})
 
-
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
